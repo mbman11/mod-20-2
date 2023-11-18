6c65187ffb6f4e0c0e83915aacd430f326ffafaf
mbman11 <m.behrmann11@gmail.com>
2023-11-17 17:59:01 -0700

    added to about page

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,21 +1,4 @@
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
 
-// const nav = {
-//   display: 'flex',
-//   height: '100px',
-//   alignItems: 'center',
-//   gap: '50px',
-//   listStyle: 'none'
-// }
-
-// const navLogo = {
-//   display: 'flex',
-//   justifyContent: 'center',
-//   background: 'blue',
-//   marginRight: '100px',
-//   width: '20%',
-// }
 
 
 function NavTabs({ currentPage, handlePageChange }) {
@@ -27,7 +10,6 @@ function NavTabs({ currentPage, handlePageChange }) {
     <a
           href="#about"
           onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage}
         >
           Logo
@@ -39,7 +21,6 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#about"
           onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage}
         >
           About
@@ -49,7 +30,6 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#portfolio"
           onClick={() => handlePageChange('Portfolio')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage}
         >
           Portfolio
@@ -59,7 +39,6 @@ function NavTabs({ currentPage, handlePageChange }) {
         <a
           href="#contact"
           onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage}
         >
           Contact
@@ -71,3 +50,7 @@ function NavTabs({ currentPage, handlePageChange }) {
 }
 
 export default NavTabs;
+
+
+
+
--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,15 +1,6 @@
 import Heading from "../UI/Heading";
 import Footer from "../UI/Footer";
-// import Img from "/images/ProfilePic.jpeg"
 
-
-// export function Img() {
-//   return (
-//     // <div id="profile-img">
-//     // </div>
-//     <img ></img>
-//   )
-// }
 
 export default function About() {
   return (
@@ -18,11 +9,43 @@ export default function About() {
       <Heading />
 
       <div className='about-section'>
-        <img src="/images/ProfilePic.png"></img>
+        <img src="/images/ProfilePic.png" id="profile-img"></img>
+        <h1>Hi, Im Max Behrmann</h1>
         <p>
-          I am a coder. Thats me. I do this.
+          I am a coder. Thats me. I do this.I started out by taking a UX/UI design course.I realized that if I knew how to code effectively I could have a well rounded baseline for a web development career.
         </p>
       </div>
+
+      <div className="button-section">
+        <a href="#education">
+          <button id="arrow-btn">Arrow</button>
+        </a>
+      </div>
+
+      <div id="education">
+        <h2>Education</h2>
+        <div className='about-section'>
+          <img src="/images/utah badge.png" id="UXbadge"></img>
+          <h3>UX/UI Bootcamp University of Utah</h3>
+          <p>
+            6 month program. I designed applications with user research and learned the basics of design principles. I really enjoyed this course because I got to interview people and test designs. It was interesting to see the user experience improve when implementing design improvements. Designing for all devices was big part of this course.
+          </p>
+        </div>
+        <div className='about-section'>
+          <img src="/images/utah badge.png" id="UXbadge"></img>
+          <h3>Full Stack Development Bootcamp <br></br>University of Utah</h3>
+          <p>
+            6 month program. I designed applications with user research and learned the basics of design principles. I really enjoyed this course because I got to interview people and test designs. It was interesting to see the user experience improve when implementing design improvements. Designing for all devices was big part of this course.
+          </p>
+        </div>
+
+      <div className="button-section">
+        <a href="#portfolio">
+          <button id="projects-btn">Projects</button>
+        </a>
+      </div>
+      </div>
+    
 
       <Footer />
     </div>
